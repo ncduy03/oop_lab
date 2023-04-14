b7b3d77c1f9b6f443e420a32a97daaa5f60e2764
ncduy03 <ncduy1906@gmail.com>
2023-04-14 10:45:50 +0700

    init

---
+++ Lab01/FirstDialog.java
@@ -0,0 +1,8 @@
+// Example 2: FirstDialog.java
+import javax.swing.JOptionPane;
+public class FirstDialog{
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Nguyen Cong Duy - 20215188 Hello world! How are you?");
+        System.exit(0);
+    }
+}
